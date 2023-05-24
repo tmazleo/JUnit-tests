497781343494c17c5974f3955c92f299ecaad875
tmazleo <leotomazb@icloud.com>
2023-05-24 19:28:42 -0300

    segundo nivel de testes

--- src/test/java/ContaTest.java
+++ src/test/java/ContaTest.java
@@ -1,58 +1,64 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ContaTest {
-
+    private Conta conta;
+    private double depositoInicial = 100.00;
+    private double valorEsperado = depositoInicial;
+    private double saque = 70.00;
+    private double saqueInvalido = 110.00;
+    private double valorEsperadoSaque = depositoInicial - saque;
+    @BeforeEach
+    public void inicializarConta(){
+        conta = new Conta();
+        conta.ativar();
+    }
     @Test
     @DisplayName("deve depositar com a conta ativa!")
     public void activatedAccountDeposit() {
-        Conta conta = new Conta();
-        conta.ativar();
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        Assertions.assertEquals(valorEsperado, conta.getSaldo());
     }
     @Test
     @DisplayName("não deve depositar com a conta inativa!")
     public void inactivatedAccountNotDeposit() {
-        Conta conta = new Conta();
+
         conta.inativar();
-        conta.depositar(100.00);
+        conta.depositar(depositoInicial);
 
         Assertions.assertEquals(0.00, conta.getSaldo());
     }
     @Test
     @DisplayName("deve sacar com a conta ativa e saldo maior que valor de saque!")
     public void activatedAccountSaqAndValueSmaller() {
-        Conta conta = new Conta();
-        conta.ativar();
-        conta.depositar(100.00);
-        conta.sacar(70.00);
 
-        Assertions.assertEquals(30.00, conta.getSaldo());
+        conta.depositar(depositoInicial);
+        conta.sacar(saque);
+
+        Assertions.assertEquals(valorEsperadoSaque, conta.getSaldo());
     }
 
     @Test
     @DisplayName("não deve sacar com a conta inativa!")
     public void inactivatedAccountNotSaq() {
-        Conta conta = new Conta();
-        conta.ativar();
-        conta.depositar(100.00);
-        conta.inativar();
-        conta.sacar(70.00);
 
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        conta.depositar(depositoInicial);
+        conta.inativar();
+        conta.sacar(saque);
+
+        Assertions.assertEquals(valorEsperado, conta.getSaldo());
     }
 
     @Test
     @DisplayName("não deve sacar com a conta ativa e saldo menor que valor de saque!")
     public void activatedAccountNotSaqAndValueSmall() {
-        Conta conta = new Conta();
-        conta.ativar();
-        conta.depositar(100.00);
-        conta.sacar(110.00);
 
-        Assertions.assertEquals(100.00, conta.getSaldo());
+        conta.depositar(depositoInicial);
+        conta.sacar(saqueInvalido);
+
+        Assertions.assertEquals(valorEsperado, conta.getSaldo());
     }
 }
